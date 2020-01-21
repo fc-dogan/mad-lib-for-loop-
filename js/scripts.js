134aa554a69cd7062cd10e14e758abaace638ef2
Jamison Cozart and Sommer Howe <sommer@>
2020-01-21 10:39:43 -0800

    refacter java scripts to use loops

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,33 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
-    var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var animalInput= $("input#animal").val();
-    var exclamationInput = $("input#exclamation").val();
-    var verbInput = $("input#verb").val();
-    var nounInput = $("input#noun").val();
+    event.preventDefault();
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
+    var inputs = ["person1","person2", "animal", "exclamation", "verb", "noun"];
+    var inputValues = [];
+    inputs.forEach(function(input) {
+      inputValues.push($("input#" + input).val());
+    })
+    inputs.forEach(function(input, index){
+      $("."+ input).text(inputValues[index])
+    
+    }) 
+
+
+    // var person1Input = $("input#person1").val();
+    // var person2Input = $("input#person2").val();
+    // var animalInput= $("input#animal").val();
+    // var exclamationInput = $("input#exclamation").val();
+    // var verbInput = $("input#verb").val();
+    // var nounInput = $("input#noun").val();
+
+
+    // $(".person1").text(person1Input);
+    // $(".person2").text(person2Input);
+    // $(".animal").text(animalInput);
+    // $(".exclamation").text(exclamationInput);
+    // $(".verb").text(verbInput);
+    // $(".noun").text(nounInput);
 
     $("#story").show();
-
-    event.preventDefault();
   });
 });
